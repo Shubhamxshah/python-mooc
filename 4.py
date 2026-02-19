d78969e6315208ffe2d6cae9de504267e84fab94
shubhamxshah <ritikgujarat@gmail.com>
2026-02-20 00:49:13 +0530

    async

--- 4.py
+++ 4.py
@@ -1,8 +1,24 @@
+import itertools
+import time
+from multiprocessing import Process, Event
+from multiprocessing import synchronize
 
+def spin(msg: str, done: synchronize.Event) -> None:
+    for char in itertools.cycle(r'\|/-'):
+        status = f'\r{char} {msg}'
+        print(status, end='', flush=True)
+        if done.wait(.1):
+            break
+    blanks = ' ' * len(status)
+    print(f'\r{blanks}\r', end='')
+
+def slow() -> int:
+    time.sleep(3)
+    return 42
 
 def supervisor() -> int:
     done = Event()
-    spinner = Thread(target=spin, args=('thinking!', done))
+    spinner = Process(target=spin, args=('thinking!', done))
     print(f'spinner object: {spinner}')
     spinner.start()
     result = slow()
